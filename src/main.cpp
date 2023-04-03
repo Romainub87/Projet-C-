369011c343b93e4b7bb905049c2139346c2591eb
Romain Cordier <rcordier@iut.u-bordeaux.fr>
2023-04-03 11:49:17 +0200

    modif main

--- src/main.cpp
+++ src/main.cpp
@@ -11,4 +11,18 @@ int main() {
     Sommet l;
     cout << n.getID() << endl;
     cout << l.getID() << endl;
+    if (n == l)
+    {
+        std::cout << "Les sommets sont égaux." << std::endl;
+    }
+    else
+    {
+        std::cout << "Les sommets sont différents." << std::endl;
+    }
+    
+    Graphe g;
+
+    g.ajouterArete(n,l);
+    cout << g.voisins(n) << endl;
+    
 }
