469338d670f144282a36aa48273fa2bc0d45e320
Romain Cordier <rcordier@iut.u-bordeaux.fr>
2023-04-05 14:43:16 +0200

    1ere etape ETAPE5 finie

--- src/main.cpp
+++ src/main.cpp
@@ -12,15 +12,25 @@ using namespace std;
 const int LARGEUR = 1200;
 const int HAUTEUR = 800;
 
+void * miseAJourAppli(void * param) {
+    Appli * app = (Appli *) param;
+    while (app->running()) {
+        app->exec();
+    }
+    pthread_exit(0);
+}
+
+
 int main(int argc, char *argv[])
 {
-    
+    pthread_t thread;
 
     if (argc != 2){
         cout << "Usage: ./app <graph_file>.txt" << endl;
         return 1;
     }
     srand(time(NULL));
+
 
     // chargement et création des graphe et fenetre d'affichage
     string fichier (argv[1]);//"graphe.txt";//"fiat.txt"
@@ -42,9 +52,8 @@ int main(int argc, char *argv[])
     cout << "Nombre d'aretes : " << g.nbAretes() << endl;
 
     // pour ne pas fermer la fenêtre après le dessin
-    while(app.running())
-        app.exec();
+    pthread_create(&thread, NULL, miseAJourAppli, &app);
+    pthread_join(thread, NULL);
 
     return 0;
 } 
-
